a7ac61ebdd2e360c5f3f4cf237fb83546c3fe36f
wouter bolsterlee <wouter@bolsterl.ee>
2018-02-16 13:31:24 +0100

    expand commentary section

--- python-pytest.el
+++ python-pytest.el
@@ -14,7 +14,24 @@
 
 ;;; Commentary:
 
-;;; This package provides helpers to run pytest.
+;; This package provides helpers to run pytest.
+;;
+;; The main command is python-pytest-popup, which will show a
+;; dispatcher menu, making it easy to change various options and
+;; switches, and then run pytest using one of the actions, which can
+;; also be run directly as commands:
+;;
+;; - python-pytest (run all tests)
+;; - python-pytest-file (current file)
+;; - python-pytest-file-dwim (‘do what i mean’ for current file)
+;; - python-pytest-function (current function)
+;; - python-pytest-function-dwim (‘do what i mean’ for current function)
+;; - python-pytest-last-failed (rerun previous failures)
+;; - python-pytest-repeat (repeat last invocation)
+;;
+;; A prefix argument causes the generated command line to be offered
+;; for editing, and various customization options influence how some
+;; of the commands work. See the README.rst for detailed information.
 
 ;;; Code:
 
