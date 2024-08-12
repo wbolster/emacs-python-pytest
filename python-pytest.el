702d52d7c41a4e0af9c4a7e3f7a047c6c9219dfd
Rodrigo Morales <rodrigo@morales.pe>
2024-08-12 14:24:14 -0500

    Declare python-pytest--current-defun obsolete
    The new functions that should be used python-pytest--path-def-at-point
    are python-pytest--path-class-at-point.
    
    python-pytest--current-defun could obtain the identifiers for
    functions and classes. If the current point was inside a function,
    there was no way to get the test id of the current class. By using
    python-pytest--path-def-at-point and
    python-pytest--path-class-at-point, there is no such limitation.

--- python-pytest.el
+++ python-pytest.el
@@ -287,7 +287,7 @@ With a prefix argument, allow editing."
   (interactive
    (list
     (buffer-file-name)
-    (python-pytest--current-defun)
+    (python-pytest--path-def-at-point)
     (transient-args 'python-pytest-dispatch)))
   (python-pytest--run
    :args args
@@ -307,7 +307,7 @@ With a prefix argument, allow editing."
   (interactive
    (list
     (buffer-file-name)
-    (python-pytest--current-defun)
+    (python-pytest--path-def-at-point)
     (transient-args 'python-pytest-dispatch)))
   (unless (python-pytest--test-file-p file)
     (setq
@@ -679,6 +679,7 @@ When present ON-REPLACEMENT is substituted, else OFF-REPLACEMENT is appended."
 
 (defun python-pytest--current-defun ()
   "Detect the current function/class (if any)."
+  (declare (obsolete 'python-pytest--path-def-at-point "python-pytest 3.5.0"))
   (let* ((name
           (or (python-info-current-defun)
               (save-excursion
